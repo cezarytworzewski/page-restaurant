36f9df0f69c401053cfcec0dbb09ce9aab986c0c
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2018-04-09 17:07:12 +0200

    Script jQuery, who add html field with info form.

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#login').click(function (event) {
+        event.preventDefault();
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var password = $('#password').val();
+        
+        $('#contact-right').html(name + '<br>' + email + '<br>' + password);
+    });
+
+});
